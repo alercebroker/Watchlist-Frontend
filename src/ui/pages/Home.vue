f3d316e6dce77b56cc8a84a5c97cd189299bf521
reyesdejong <reyes.dejong@gmail.com>
2021-05-31 12:30:44 -0400

    feat(targets): everuthing broken git push

--- src/ui/pages/Home.vue
+++ src/ui/pages/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <v-container fill-height>
+    <a-header title="ALeRCE Watchlist" @loggedout="logout"></a-header>
+    {{logged}}
     <v-row
-      v-if="!logged"
+      v-if="!log_var"
       align="center"
       align-content="center"
       justify="center"
@@ -30,20 +32,33 @@ import MyWatchlists from "@/ui/components/watchlist/MyWatchlists.vue";
 import WatchlistDetails from "../components/watchlist/WatchlistDetails.vue";
 import Login from "../components/users/Login.vue";
 import RegisterUser from "../components/users/RegisterUser.vue";
+import AHeader from "@/ui/components/watchlist/AHeader.vue";
 
 export default Vue.extend({
-  components: { MyWatchlists, WatchlistDetails, Login, RegisterUser },
+  components: { AHeader, MyWatchlists, WatchlistDetails, Login, RegisterUser },
   data() {
     return {
       dialog: false,
       watchlist_dialog: false,
+      log_var: false,
     };
   },
   computed: {
     logged() {
       const token = localStorage.getItem("access_token");
       const user = this.$store.state.users.userData;
+      if (token != null && user.username !== "") {
+          this.log_var = true;
+      }
       return token != null && user.username !== "";
+    },
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("access_token");
+      const token = localStorage.getItem("access_token");
+      this.log_var = false;
+      console.log(token)
     },
   },
 });
