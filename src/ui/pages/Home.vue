d7b37ec1af732ca0c47152c96b3eea0f7280de95
Javier <javier.arredondo.c@usach.cl>
2021-08-03 14:25:41 -0400

    ADD: Alert after register that show message for look at user email

--- src/ui/pages/Home.vue
+++ src/ui/pages/Home.vue
@@ -3,15 +3,18 @@
     <a-header title="ALeRCE Watchlist"></a-header>
     <v-row v-if="!logged" align="center" align-content="center">
       <v-col cols="4">
-        <login @registerClick="dialog = true" />
-        <v-dialog v-model="dialog" max-width="500">
-          <register-user @registered="dialog = false" />
+        <login
+          @registerClick="registerDialog = true"
+          :afterRegister="afterRegister"
+        />
+        <v-dialog v-model="registerDialog" max-width="500">
+          <register-user @registered="registered" />
         </v-dialog>
       </v-col>
     </v-row>
     <v-row v-else>
       <v-col xs="12" lg="3">
-        <my-watchlists @createWatchlist="watchlist_dialog = true" />
+        <my-watchlists @createWatchlist="watchlistDialog = true" />
       </v-col>
       <v-col xs="12" lg="9">
         <watchlist-details />
@@ -32,15 +35,22 @@ import { IUserData } from "@/app/user/domain/User.types";
 export default Vue.extend({
   components: { AHeader, MyWatchlists, WatchlistDetails, Login, RegisterUser },
   data: (): {
-    dialog: boolean;
-    watchlist_dialog: boolean;
+    registerDialog: boolean;
+    watchlistDialog: boolean;
     accessToken: string | null;
+    afterRegister: boolean;
   } => ({
-    dialog: false,
-    watchlist_dialog: false,
+    registerDialog: false,
+    watchlistDialog: false,
     accessToken: null,
+    afterRegister: false,
   }),
-
+  methods: {
+    registered() {
+      this.afterRegister = true;
+      this.registerDialog = false;
+    },
+  },
   computed: {
     logged: function (): boolean {
       return this.accessToken != null;
