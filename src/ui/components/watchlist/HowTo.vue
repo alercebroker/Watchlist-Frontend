e0dbaa23a09d04abea5a41b4adc94d7c6c435a45
DiegoMellisG <diego.mellis@usach.cl>
2021-08-04 10:49:38 -0400

    ADD: set v-card inside v-container to center the content

--- src/ui/components/watchlist/HowTo.vue
+++ src/ui/components/watchlist/HowTo.vue
@@ -1,44 +1,46 @@
 <template>
-  <v-card class="mx-auto">
-    <v-card-title>How to upload CSV</v-card-title>
-    <v-card-text>
-      For upload your targets with a CSV, you need respect some formats.
-    </v-card-text>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>1. The header must be: name, ra, dec and radius, all in lower case.</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+  <v-container>
+    <v-card class="mx-auto">
+      <v-card-title>How to upload CSV</v-card-title>
+      <v-card-text>
+        For upload your targets with a CSV, you need respect some formats.
+      </v-card-text>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>1. The header must be: name, ra, dec and radius, all in lower case.</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>2. Ra and dec must be in degrees, radius in arcsec</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>2. Ra and dec must be in degrees, radius in arcsec</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>3. The values of the fields must be separated by comas.</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>3. The values of the fields must be separated by comas.</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>4. The table below shows the fields and the accepted values.</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-data-table
-    :headers="headers"
-    :items="fields"
-    :items-per-page="5"
-    class="elevation-1"
-    ></v-data-table>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>5. You can see <a href="https://alerce-static.s3.amazonaws.com/targets_example/target_sample.csv"> this example</a> to understand the format.</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-  </v-card>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>4. The table below shows the fields and the accepted values.</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-data-table
+      :headers="headers"
+      :items="fields"
+      :items-per-page="5"
+      class="elevation-1"
+      ></v-data-table>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>5. You can see <a href="https://alerce-static.s3.amazonaws.com/targets_example/target_sample.csv"> this example</a> to understand the format.</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
