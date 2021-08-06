04d5433308e182f7832af40235fefbbc7905fe0c
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-08-06 10:07:53 -0400

    delete mocks

--- src/ui/app.container.ts
+++ src/ui/app.container.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
-import { cid, container, mockSingleton } from "inversify-props";
-import { HttpService, MockUserApi, TestActions } from "@/shared/http";
+import { container } from "inversify-props";
+import { HttpService } from "@/shared/http";
 import { IWatchlistRepository } from "@/app/watchlist/domain";
 import { WatchlistService } from "@/app/watchlist/infrastructure/WatchlistService";
 import { GetAllWatchlists } from "@/app/watchlist/use_case/GetAllWatchlists";
@@ -40,6 +40,4 @@ export function containerBuilder(): void {
   container.addSingleton<UseCaseInteractor>(GetMatches);
   container.bind<Modules>("Modules").toConstantValue(modules);
   container.addSingleton<IStoreCreator>(StoreCreator);
-  // container.bind<TestActions>("ActionType").toConstantValue("clientError");
-  // mockSingleton<HttpService>(cid.UsersApiService, MockUserApi);
 }
