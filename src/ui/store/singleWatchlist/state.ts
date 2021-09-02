3280738111c3194332f88de18f0196f2fd8d5423
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-09-02 10:08:14 -0400

    Update state.ts

--- src/ui/store/singleWatchlist/state.ts
+++ src/ui/store/singleWatchlist/state.ts
@@ -5,7 +5,7 @@ export const state = () => ({
   n_targets: "",
   last_match: "",
   title: "",
-  notification_rate: "",
+  notification_rate: "disabled",
   error: null as string | null,
   targets: "",
 });
