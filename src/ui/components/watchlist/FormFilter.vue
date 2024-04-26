e211edf345aac2ab6af141226d4789fc538a6266
Demurest <eric.apolonio@hotmail.com>
2024-04-26 09:57:10 -0400

    visual changes dialog

--- src/ui/components/watchlist/FormFilter.vue
+++ src/ui/components/watchlist/FormFilter.vue
@@ -75,8 +75,7 @@
       <v-card :loading="loading_singleWatchlist">
         <v-card-title class="text-h5">
           <span
-            >¿Are you sure you want to submit it to all the filters of the
-            watchlist?</span
+            >¿Are you sure you want to change all the filters in the watchlist?</span
           >
         </v-card-title>
 
